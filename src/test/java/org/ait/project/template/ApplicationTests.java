8f2e526f025ac3c87184d229954cdd482f96cae4
saldhyyogaprathama <107382713+saldhyyogaprathama@users.noreply.github.com>
2022-12-28 14:19:41 +0700

    Initial commit

---
+++ src/test/java/org/ait/project/template/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ait.project.template;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+    Assertions.assertEquals(true,true);
+  }
+
+}
